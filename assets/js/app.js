6562fc2f28c86a20105c36a27458c93e25156f45
MyUsername <romamargine@gmail.com>
2022-04-29 16:11:15 +0200

    Add es email

---
+++ assets/js/app.js
@@ -0,0 +1,37 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    email: "",
+    emails: [],
+  },
+  methods: {
+    // pushingEmails(email) {
+    //   for (let i = 1; i <= 10; i++) {
+    //     if (!this.emails.includes(email)) {
+    //       this.emails.push(email);
+    //     } else {
+    //       return;
+    //     }
+    //   }
+    // },
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => {
+        // console.log(this);
+        // console.log(response);
+        // console.log(this.email);
+        this.email = response.data.response
+        if (this.email !== this.emails) {
+          for(let k = 0; k <= 10; k++) {
+            this.emails.push(this.email);
+          }
+        } else {
+            return
+        }
+
+        console.log(this.emails);
+      });
+  },
+});
