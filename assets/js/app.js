74ba3b8d8636722241759f048a744ae605b5dd62
MyUsername <romamargine@gmail.com>
2022-04-29 16:26:27 +0200

    Add complete ex

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,23 +16,23 @@ const app = new Vue({
     // },
   },
   mounted() {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((response) => {
-        // console.log(this);
-        // console.log(response);
-        // console.log(this.email);
-        this.email = response.data.response;
+    for (let k = 0; k <= 10; k++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          // console.log(this);
+          // console.log(response);
+          // console.log(this.email);
+          this.email = response.data.response;
 
-        for (let k = 0; k <= 10; k++) {
-          if (this.email != this.emails[k]) {
+          if (this.email[k] !== this.emails[k]) {
             this.emails.push(this.email);
           } else {
             return;
           }
-        }
 
-        console.log(this.emails);
-      });
+          console.log(this.emails);
+        });
+    }
   },
 });
